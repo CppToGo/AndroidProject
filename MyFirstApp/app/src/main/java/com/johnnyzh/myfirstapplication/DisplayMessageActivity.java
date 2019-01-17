1e145393f69be7b078f0fde3957fd963ce0b53f8
johnny <1814607825@qq.com>
2019-01-17 17:43:46 +0800

    '增添指纹验证功能：不完善；侧面滑动（全面屏）也会取消指纹验证'

--- MyFirstApp/app/src/main/java/com/johnnyzh/myfirstapplication/DisplayMessageActivity.java
+++ MyFirstApp/app/src/main/java/com/johnnyzh/myfirstapplication/DisplayMessageActivity.java
@@ -3,6 +3,8 @@ package com.johnnyzh.myfirstapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -19,5 +21,11 @@ public class DisplayMessageActivity extends AppCompatActivity {
         //Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- MyFirstApp/app/src/main/java/com/johnnyzh/myfirstapplication/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/johnnyzh/myfirstapplication/MyActivity.java
@@ -1,10 +1,15 @@
 package com.johnnyzh.myfirstapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.hardware.biometrics.BiometricPrompt;
+import android.os.CancellationSignal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toolbar;
@@ -12,10 +17,65 @@ import android.widget.Toolbar;
 public class MyActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.johnnyzh.myfirstapplication.MESSAGE";
+
+
+    /**
+     * 生物识别代码部分
+     */
+    private static final String TAG = "gryphon";
+    private BiometricPrompt mBiometricPrompt;
+    private CancellationSignal mCancellationSignal;
+    private BiometricPrompt.AuthenticationCallback mAuthenticationCallback;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+
+        mBiometricPrompt = new BiometricPrompt.Builder(this)
+                .setTitle("指纹验证")
+                .setDescription("描述")
+                .setNegativeButton("取消", getMainExecutor(), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        Log.i(TAG , "Cancel button clicked");
+                    }
+                }).build();
+        mCancellationSignal = new CancellationSignal();
+        mCancellationSignal.setOnCancelListener(new CancellationSignal.OnCancelListener() {
+            @Override
+            public void onCancel() {
+                //handle cancel result
+                finish();
+                Log.i(TAG , "Canceled");
+            }
+        });
+
+        mAuthenticationCallback = new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Log.i(TAG , "onAuthenticationError" + errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+
+                Log.i(TAG , "onAuthenticationSucceeded" + result.toString());
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Log.i(TAG , "onAuthenticationFailed");
+            }
+        };
+
+        mBiometricPrompt.authenticate(mCancellationSignal , getMainExecutor() , mAuthenticationCallback);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -29,5 +89,20 @@ public class MyActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE , message);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //处理动作按钮的点击事件
+        switch (item.getItemId()){
+            case R.id.app_bar_search:
+                //openSearch();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 }
