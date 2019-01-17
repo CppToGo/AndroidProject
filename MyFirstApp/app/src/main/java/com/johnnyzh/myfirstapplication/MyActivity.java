d172a7ed56e9edb988f5f8c9805092171da0fd87
johnny <1814607825@qq.com>
2019-01-17 10:59:54 +0800

    添加menu
    Signed-off-by: johnny <1814607825@qq.com>

--- MyFirstApp/app/src/main/java/com/johnnyzh/myfirstapplication/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/johnnyzh/myfirstapplication/MyActivity.java
@@ -3,8 +3,11 @@ package com.johnnyzh.myfirstapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toolbar;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -13,6 +16,12 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
     public void sendMessage(View view){
         Intent intent = new Intent(this, DisplayMessageActivity.class);
